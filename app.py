d4ea6f7b2221fa0b872986ba55109db187738504
Danyllo Danesi <danyllo.danesi@gmail.comgit config --global user.name Danyllo>
2024-05-24 11:15:17 -0300

    atualizacao final

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-# Descrever os passos manuais e  depois transformar isso em codigo
-# Ler planilhas e guardar informações sobre nome, telefone e data de vencimento
-# Criar links personalizados do whatapp e enviar mensagens para cada cliente com base nos dados da planilha
-
+"""
+PRECISO AUTOMATIZAR MINHAS MENSAGENS P/ MEUS CLIENTES GOSTARIA DE SABER VALORES, E GOSTARIA QUE ENTRASSEM EM CONTATO COMIGO P/ EXPLICAR MELHOR, QUERO PODER MANDAR MENSAGENS DE COBRANÇA EM DETERMINADO DIA COM CLIENTES COM VENCIMENTO DIFERENTE
+"""
 import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+import pyautogui
+import os 
 
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
 
-workbook= openpyxl.load_workbook('clientes.xlsx')
+# Ler planilha e guardar informações sobre nome, telefone e data de vencimento
+workbook = openpyxl.load_workbook('clientes.xlsx')
 pagina_clientes = workbook['Sheet1']
 
 for linha in pagina_clientes.iter_rows(min_row=2):
-    #extrair nome, telefone, vencimento
+    # nome, telefone, vencimento
     nome = linha[0].value
     telefone = linha[1].value
     vencimento = linha[2].value
-    print(nome, telefone, vencimento)
+    
+    mensagem = f'Olá {nome} seu boleto vence no dia {vencimento}. Favor pagar no link https://www.linkdopagamento.com'
+
+    # Criar links personalizados do whatsapp e enviar mensagens para cada cliente
+    # com base nos dados da planilha
+    try:
+        link_mensagem_whatsapp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+        webbrowser.open(link_mensagem_whatsapp)
+        sleep(10)
+        seta = pyautogui.locateCenterOnScreen('seta1.png')
+        sleep(8)
+        pyautogui.click(seta[0],seta[1])
+        sleep(8)
+        pyautogui.hotkey('ctrl','w')
+        sleep(8)
+    except:
+        print(f'Não foi possível enviar mensagem para {nome}')
+        with open('erros.csv','a',newline='',encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome},{telefone}{os.linesep}')
+            pyautogui.hotkey('ctrl','w')
+            sleep(8)
